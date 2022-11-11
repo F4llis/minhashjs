92663034ad799b3d574c7af31bf2cb9a8faaa50b
F4llis <clement.train@gmail.com>
2022-11-11 10:47:15 +0100

    Ut forest: Add accuracy tests

--- ForestLSH.test.js
+++ ForestLSH.test.js
@@ -1,6 +1,14 @@
 
 const F = require('./ForestLSH.js');
 const MH = require("./MinHash");
+const data = require("./test.accuraccy.json");
+
+function getMultipleRandom(arr, num) {
+    const shuffled = [...arr].sort(() => 0.5 - Math.random());
+
+    return shuffled.slice(0, num);
+}
+
 
 test('Init Forest', () => {
     var forest = new F.MinHashLSHForest()
@@ -11,7 +19,7 @@ test('Init Forest', () => {
 
 });
 
-test('Init Forest', () => {
+test('Build Forest', () => {
 
     var data1 = ['minhash', 'is', 'a', 'probabilistic', 'data', 'structure', 'for',
         'estimating', 'the', 'similarity', 'between', 'datasets']
@@ -47,14 +55,11 @@ test('Init Forest', () => {
 
 
     // Using m1 as the query, retrieve top 2 keys that have the higest Jaccard
-    var result = forest.query(m1, 1)
-    console.log("Top 2 candidates", result)
+    forest.query(m1, 1)
 
 });
 
-
-
-test('Query', () => {
+test('Query item already inside forest', () => {
 
     const data = require('./test.accuraccy.json');
     const repo = {}
@@ -63,7 +68,7 @@ test('Query', () => {
     var forest = new F.MinHashLSHForest(num_perm=128)
 
     cpt=0
-    for (var i = 0; i < data.datum.length-2; i++){
+    for (var i = 0; i < data.datum.length; i++){
 
         data1 = data.datum[i][0]
         data2 = data.datum[i][1]
@@ -84,17 +89,73 @@ test('Query', () => {
 
     }
 
-
     forest.index()
 
     mx = new MH.MinHash()
-    var daty = data.datum[data.datum.length-1][0]
+    var daty =  repo["m10"][0]
     daty.forEach((d, i) => mx.update(d));
 
 
 
-    var result = forest.query(mx, 2)
-    console.log("Top X candidates", result)
-    console.log(mx.jaccard(repo[result[0]][1]))
+    var r  = forest.query(mx, 2)
+
+    expect(r.includes('m10')).toBeTruthy();
+
+})
+
+test('Query accuracy of custom query', () => {
+
+    const data = require('./test.accuraccy.json');
+    const repo = {}
+
+
+    var forest = new F.MinHashLSHForest(num_perm=128)
+
+    cpt=0
+    for (var i = 0; i < data.datum.length; i++){
+
+        data1 = data.datum[i][0]
+        data2 = data.datum[i][1]
+
+        m1 = new MH.MinHash()
+        data1.forEach((d, i) => m1.update(d));
+
+        m2 = new MH.MinHash()
+        data2.forEach((d, i) => m2.update(d));
+
+        forest.add("m" + cpt, m1)
+        repo["m" + cpt] = [data1,m1]
+        cpt += 1
+
+        forest.add("m" + cpt, m2)
+        repo["m" + cpt] = [data2, m2]
+        cpt += 1
+
+    }
+    forest.index()
+
+    mx = new MH.MinHash()
+
+
+    var keys = getMultipleRandom(Object.keys(repo), 2)
+
+    var first = repo[keys[0]]
+    var second = repo[keys[1]]
+
+    var half = Math.ceil(first[0].length / 2);
+
+
+    var first_half = first[0].splice(0, half)
+    var second_half = second[0].splice(-half)
+
+    var daty = first_half.concat(second_half)
+
+    daty.forEach((d, i) => mx.update(d));
+
+    var r = forest.query(mx, 10)
+
+    expect(r.includes(keys[0])).toBeTruthy();
+    expect(r.includes(keys[1])).toBeTruthy();
+
 
 })
